93fb35cf67af7dd9a831954c41cd9872a4201f1c
João <oliveira.joaovtr@gmail.com>
2024-01-13 13:29:58 -0300

    Criação das classes dos nós de 2 e 3 conexões

--- fluidim/sistema/nos.py
+++ fluidim/sistema/nos.py
@@ -4,3 +4,24 @@ class No:
 
         self.cota = 0
         self.pressao = 0
+
+
+class No2Conexoes(No):
+    def __init__(self, nome):
+        super().__init__(nome)
+        self.conexao_1 = None
+        self.conexao_2 = None
+
+        self.comprimento_equivalente_1_2 = 0
+
+
+class No3Conexoes(No):
+    def __init__(self, nome):
+        super().__init__(nome)
+        self.conexao_1 = None
+        self.conexao_2 = None
+        self.conexao_3 = None
+
+        self.comprimento_equivalente_1_2 = 0
+        self.comprimento_equivalente_1_3 = 0
+        self.comprimento_equivalente_2_3 = 0
